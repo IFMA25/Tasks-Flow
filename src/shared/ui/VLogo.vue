4e32c15df2903198f49d76832ad668ed74a8d8e0
IFMA25 <innamassura@gmail.com>
2026-05-11 23:42:40 +0300

    fix: src log

--- src/shared/ui/VLogo.vue
+++ src/shared/ui/VLogo.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center gap-2">
     <img
       class="w-[1.75rem] h-[1.75rem]"
-      src="../assets/icons/logo.svg"
+      src="@/shared/assets/icons/logo.svg"
       alt="logo"
     >
     <p class="text-lg font-bold text-title">
